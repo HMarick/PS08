f940e110a72f0f0d6097dbceb32f063ecea63602
Harrison Marick <hmarick17@amherst.edu>
2017-10-27 20:00:29 -0400

    yup

--- PS08.R
+++ PS08.R
@@ -41,15 +41,31 @@ runtime_dataframe
 
 
 # Time knn here -----------------------------------------------------------
-
-
-
+n_values <- c(10000, 20000, 30000, 5000, 1000, 100000, 50000, 25000, 75000)
+k_values <- c(1, 3, 5, 7, 9, 11, 13, 15)
+runtime<-rep(0, length(n_values)*length(k_values))
+k<-rep(0,length(n_values)*length(k_values)) #storage for k values
+n<-rep(0,length(n_values)*length(k_values)) #storage for n values
+count=1
+for (j in 1:length(k_values)){ #iterate through vals of k
+for (i in 1:length(n_values)){ #iterate through vals of n
+  tic()
+  m1<-caret::knn3(model_formula, data=slice(train, 1:n_values[i]), k=k_values[j]) #create model
+  timer_info=toc()
+  runtime[count] <- timer_info$toc - timer_info$tic
+  k[count]=k_values[j] #store k-val
+  n[count]=n_values[i] #store n-val
+  count=count+1
+}
+}
+times<-data.frame(n=n, k=k, runtime=runtime)
 
 # Plot your results ---------------------------------------------------------
 # Think of creative ways to improve this barebones plot. Note: you don't have to
 # necessarily use geom_point
-runtime_plot <- ggplot(runtime_dataframe, aes(x=n, y=k, col=runtime)) +
-  geom_point()
+runtime_plot <- ggplot(times, aes(x=n*k, y=runtime)) +
+  geom_point() + ggtitle("Runtime by N*K") +xlab("N*K") + ylab("Runtime")
+#I did not really like the colored plot. I did not think it was very helpful, so I changed the plot.
 
 runtime_plot
 ggsave(filename="firstname_lastname.png", width=16, height = 9)
@@ -63,5 +79,7 @@ ggsave(filename="firstname_lastname.png", width=16, height = 9)
 # -n: number of points in training set
 # -k: number of neighbors to consider
 # -d: number of predictors used? In this case d is fixed at 3
+
+
 
 
